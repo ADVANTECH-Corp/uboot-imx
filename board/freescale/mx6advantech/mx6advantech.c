41e2de67a5e2b9f17f2279cf5bfe3e86cdfc9dc6
timliangtw <tim.liang@advantech.com.tw>
2016-11-29 15:00:32 +0800

    0036_project_001_add_rom-5420_solo_512MB_config

--- board/freescale/mx6advantech/mx6advantech.c
+++ board/freescale/mx6advantech/mx6advantech.c
@@ -1375,6 +1375,65 @@ const struct mx6dq_iomux_ddr_regs mx6_ddr_ioregs = {
 	.dram_dqm7 =  0x00020030,
 };
 
+/* configure MX6SOLO/DUALLITE mmdc DDR io registers */
+const struct mx6sdl_iomux_ddr_regs mx6sdl_ddr_ioregs = {
+        /* SDCLK[0:1], CAS, RAS, Reset: Differential input, 40ohm */
+        .dram_sdclk_0 = 0x00020030,
+        .dram_sdclk_1 = 0x00020030,
+        .dram_cas = 0x00020030,
+        .dram_ras = 0x00020030,
+        .dram_reset = 0x00020030,
+        /* SDCKE[0:1]: 100k pull-up */
+        .dram_sdcke0 = 0x00003000,
+        .dram_sdcke1 = 0x00003000,
+        /* SDBA2: pull-up disabled */
+        .dram_sdba2 = 0x00000000,
+        /* SDODT[0:1]: 100k pull-up, 40 ohm */
+        .dram_sdodt0 = 0x00003030,
+        .dram_sdodt1 = 0x00003030,
+        /* SDQS[0:7]: Differential input, 40 ohm */
+        .dram_sdqs0 = 0x00000030,
+        .dram_sdqs1 = 0x00000030,
+        .dram_sdqs2 = 0x00000030,
+        .dram_sdqs3 = 0x00000030,
+        .dram_sdqs4 = 0x00000030,
+        .dram_sdqs5 = 0x00000030,
+        .dram_sdqs6 = 0x00000030,
+        .dram_sdqs7 = 0x00000030,
+
+        /* DQM[0:7]: Differential input, 40 ohm */
+        .dram_dqm0 = 0x00020030,
+        .dram_dqm1 = 0x00020030,
+        .dram_dqm2 = 0x00020030,
+        .dram_dqm3 = 0x00020030,
+        .dram_dqm4 = 0x00020030,
+        .dram_dqm5 = 0x00020030,
+        .dram_dqm6 = 0x00020030,
+        .dram_dqm7 = 0x00020030,
+};
+
+const struct mx6sdl_iomux_grp_regs mx6sdl_grp_ioregs = {
+        /* DDR3 */
+        .grp_ddr_type = 0x000c0000,
+        /* SDQS[0:7]: Differential input, 40 ohm */
+        .grp_ddrmode_ctl = 0x00020000,
+        /* disable DDR pullups */
+        .grp_ddrpke = 0x00000000,
+        /* ADDR[00:16], SDBA[0:1]: 40 ohm */
+        .grp_addds = 0x00000030,
+        /* CS0/CS1/SDBA2/CKE0/CKE1/SDWE: 40 ohm */
+        .grp_ctlds = 0x00000030,
+        /* DATA[00:63]: Differential input, 40 ohm */
+        .grp_ddrmode = 0x00020000,
+        .grp_b0ds = 0x00000030,
+        .grp_b1ds = 0x00000030,
+        .grp_b2ds = 0x00000030,
+        .grp_b3ds = 0x00000030,
+        .grp_b4ds = 0x00000030,
+        .grp_b5ds = 0x00000030,
+        .grp_b6ds = 0x00000030,
+        .grp_b7ds = 0x00000030,
+};
 const struct mx6dq_iomux_ddr_regs mx6dqp_ddr_ioregs = {
 	.dram_sdclk_0 =  0x00000030,
 	.dram_sdclk_1 =  0x00000030,
@@ -1524,9 +1583,13 @@ static void spl_dram_init(void)
 		mx6dq_dram_iocfg(64, &mx6dqp_ddr_ioregs, &mx6_grp_ioregs);
 		mx6_dram_cfg(&sysinfo, &mx6dqp_mmcd_calib, &mem_ddr);
 	} else {
+#ifdef CONFIG_MX6Q
 		mx6dq_dram_iocfg(64, &mx6_ddr_ioregs, &mx6_grp_ioregs);
-	mx6_dram_cfg(&sysinfo, &mx6_mmcd_calib, &mem_ddr);
-}
+#elif defined(CONFIG_MX6DL) 
+		mx6sdl_dram_iocfg(32, &mx6sdl_ddr_ioregs, &mx6sdl_grp_ioregs);
+#endif
+		mx6_dram_cfg(&sysinfo, &mx6_mmcd_calib, &mem_ddr);
+	}
 }
 
 void board_init_f(ulong dummy)
@@ -1640,6 +1703,7 @@ void spi_io_init(struct imx_spi_dev_t *dev)
                 /* ecspi2-3 fall through */
                 break;
 #ifdef CONFIG_ADVANTECH
+#ifdef CONFIG_MX6Q
 #ifdef CONFIG_SPI_BOOT
         case ECSPI5_BASE_ADDR:
                 reg = readl(&ccm_regs->CCGR1);
@@ -1656,8 +1720,9 @@ void spi_io_init(struct imx_spi_dev_t *dev)
                 imx_iomux_v3_setup_pad(IOMUX_SPI5_CS2  | MUX_PAD_CTRL(NO_PAD_CTRL));
 #endif
                 break;
-#endif
-#endif
+#endif //CONFIG_SPI_BOOT
+#endif //CONFIG_MX6Q
+#endif //CONFIG_ADVANTECH
         default:
                 break;
         }
