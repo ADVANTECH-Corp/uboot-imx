b0fc3aa39aae79e3ed9cf273ba72b21d459de788
Tim Liang <tim.liang@advantech.com.tw>
2018-04-25 14:00:42 +0800

    [EBC-RS03]Digital output pull low in SPL

--- board/freescale/mx6advantech/mx6advantech.c
+++ board/freescale/mx6advantech/mx6advantech.c
@@ -1039,9 +1039,6 @@ int board_init(void)
 #if defined (CONFIG_ADVANTECH) && defined(CONFIG_SUPPORT_LVDS)
        setup_lvds_init();
 #endif
-#ifdef	DIGITAL_OUTPUT
-	setup_do_init();
-#endif
 #ifdef CONFIG_MXC_SPI
 	setup_spi();
 #endif
@@ -1819,6 +1816,10 @@ void board_init_f(ulong dummy)
 	/* iomux and setup of i2c */
 	board_early_init_f();
 
+#ifdef	DIGITAL_OUTPUT
+	setup_do_init();
+#endif
+
 	/* setup GP timer */
 	timer_init();
 
@@ -1831,7 +1832,6 @@ void board_init_f(ulong dummy)
 #else
 	spl_dram_init();
 #endif
-
 	/* Clear the BSS. */
 	memset(__bss_start, 0, __bss_end - __bss_start);
 
