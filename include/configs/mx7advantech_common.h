e8a36b3c28e8503efd18b97baaf303f1859b2c97
alex6235 <alex.cheng@advantech.com.tw>
2018-09-12 16:36:28 +0800

    add EBC-RM01 project - Only support DDR 1G for IM and Micron at present

---
+++ include/configs/mx7advantech_common.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015-2016 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Freescale i.MX7.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+/*
+#ifndef __MX7_COMMON_H
+#define __MX7_COMMON_H
+*/
+
+#ifndef __MX7D_SABRESD_COMMON_CONFIG_H
+#define __MX7D_SABRESD_COMMON_CONFIG_H
+
+#include "mx7_common.h"
+
+/* #define CONFIG_ADVANTECH */
+
+#define CONFIG_PHYLIB
+#define CONFIG_PHY_ATHEROS
+
+
+/* boot device dev number */
+#define	CONFIG_EMMC_DEV_NUM	1
+#define	CONFIG_SD_DEV_NUM	0
+
+#endif
