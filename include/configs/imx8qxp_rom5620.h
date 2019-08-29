33423b1b11998f956802cb915950cc205b353858
Daniel Hung <daniel.hung@advantech.com.tw>
2019-08-29 09:37:11 +0800

    [ROM-5620] Set env for LVDS panels

--- include/configs/imx8qxp_rom5620.h
+++ include/configs/imx8qxp_rom5620.h
@@ -232,6 +232,9 @@
 	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
 	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
 	"loadcntr=fatload mmc ${mmcdev}:${mmcpart} ${cntr_addr} ${cntr_file}\0" \
+	"lvds_chimei=setenv fdt_file adv-imx8qxp-rom5620-a1-lvds-chimei.dtb; boot\0" \
+	"lvds_dual=setenv fdt_file adv-imx8qxp-rom5620-a1-lvds-dual.dtb; boot\0" \
+	"hdmi_bridge=setenv fdt_file adv-imx8qxp-rom5620-a1-hdmi-bridge.dtb; boot\0" \
 	"auth_os=auth_cntr ${cntr_addr}\0" \
 	"boot_os=booti ${loadaddr} - ${fdt_addr};\0" \
 	"mmcboot=echo Booting from mmc ...; " \
