dd7359ebafec30d893580982b426aacec4da8dab
timliangtw <tim.liang@advantech.com.tw>
2020-04-23 17:55:07 +0800

    [ROM-5721]modify for DVT

--- board/freescale/imx8mm_rom5721a1/imx8mm_rom5721a1.c
+++ board/freescale/imx8mm_rom5721a1/imx8mm_rom5721a1.c
@@ -38,12 +38,30 @@ DECLARE_GLOBAL_DATA_PTR;
 static iomux_v3_cfg_t const uart_pads[] = {
 	IMX8MM_PAD_SAI3_TXC_UART2_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
 	IMX8MM_PAD_SAI3_TXFS_UART2_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
-
 };
 
 static iomux_v3_cfg_t const wdog_pads[] = {
 	IMX8MM_PAD_GPIO1_IO02_WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
 };
+
+static iomux_v3_cfg_t debug_uart_sel_gpio[] = {
+	IMX8MM_PAD_SAI1_RXC_GPIO4_IO1 | MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static void debug_uart_sel(void)
+{
+	int value = -1;
+
+	imx_iomux_v3_setup_multiple_pads(debug_uart_sel_gpio, ARRAY_SIZE(debug_uart_sel_gpio));
+	gpio_request(DEBUG_UART_SEL, "debug_uart_sel");
+	gpio_direction_input(DEBUG_UART_SEL);
+
+	value = gpio_get_value(DEBUG_UART_SEL);
+
+	/* High: enable debug log. Low: disable debug log. */
+	if(value == 0)
+		env_set("console", "disabled");
+}
 
 #ifdef CONFIG_FSL_FSPI
 #define QSPI_PAD_CTRL	(PAD_CTL_DSE2 | PAD_CTL_HYS)
@@ -171,11 +189,11 @@ int ft_board_setup(void *blob, bd_t *bd)
 }
 #endif
 
-#define LVDS_STBY_PAD IMX_GPIO_NR(3, 23)
-#define LVDS_RESET_PAD IMX_GPIO_NR(3, 24)
 static iomux_v3_cfg_t const gpio_init_pads[] = {
         IMX8MM_PAD_SAI5_RXD2_GPIO3_IO23 | MUX_PAD_CTRL(NO_PAD_CTRL),	//LVDS_STBY
         IMX8MM_PAD_SAI5_RXD3_GPIO3_IO24 | MUX_PAD_CTRL(NO_PAD_CTRL),	//LVDS_RESET
+		IMX8MM_PAD_GPIO1_IO10_GPIO1_IO10 | MUX_PAD_CTRL(NO_PAD_CTRL),	//I2S_EN
+		IMX8MM_PAD_SAI3_RXFS_GPIO4_IO28 | MUX_PAD_CTRL(NO_PAD_CTRL),	//RESET_OUT
 };
 
 static void setup_iomux_gpio(void)
@@ -185,15 +203,21 @@ static void setup_iomux_gpio(void)
 
         gpio_request(LVDS_STBY_PAD, "LVDS_STBY");
         gpio_direction_output(LVDS_STBY_PAD, 1);
-	udelay(100);
+		udelay(100);	//for lvds init sequence
         gpio_request(LVDS_RESET_PAD, "LVDS_RESET");
         gpio_direction_output(LVDS_RESET_PAD, 1);
+
+		gpio_request(I2S_EN, "I2S_EN");
+        gpio_direction_output(I2S_EN, 1);
+
+		gpio_request(RESET_OUT, "RESET_OUT");
+        gpio_direction_output(RESET_OUT, 1);
 }
 
 void setup_iomux_wdt()
 {
-        imx_iomux_v3_setup_pad(IOMUX_WDOG_TRIG| MUX_PAD_CTRL(NO_PAD_CTRL));
-        imx_iomux_v3_setup_pad(IOMUX_WDOG_ENABLE| MUX_PAD_CTRL(NO_PAD_CTRL));
+        imx_iomux_v3_setup_pad(IMX8MM_PAD_GPIO1_IO15_GPIO1_IO15| MUX_PAD_CTRL(NO_PAD_CTRL));
+        imx_iomux_v3_setup_pad(IMX8MM_PAD_GPIO1_IO09_GPIO1_IO9| MUX_PAD_CTRL(NO_PAD_CTRL));
         gpio_request(WDOG_ENABLE, "wdt_en");
         gpio_direction_output(WDOG_ENABLE,0);
         gpio_request(WDOG_TRIG, "wdt_trig");
@@ -755,6 +779,7 @@ int board_late_init(void)
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif
+	debug_uart_sel();
 
 	return 0;
 }
