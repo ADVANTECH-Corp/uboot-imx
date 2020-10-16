41feaa5f0d98d271afcbff388a30f59922685bf0
Darren Huang <darren.huang@advantech.com.tw>
2020-10-16 16:22:39 +0800

    [RSB-3720] Change the debug console to UART3.
    	Need to change the imx-atf(bl31.bin) debug config, too.

--- board/freescale/imx8mp_rsb3720a1/imx8mp_rsb3720a1.c
+++ board/freescale/imx8mp_rsb3720a1/imx8mp_rsb3720a1.c
@@ -28,8 +28,8 @@ DECLARE_GLOBAL_DATA_PTR;
 #define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
 
 static iomux_v3_cfg_t const uart_pads[] = {
-	MX8MP_PAD_UART2_RXD__UART2_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
-	MX8MP_PAD_UART2_TXD__UART2_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	MX8MP_PAD_ECSPI1_SCLK__UART3_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	MX8MP_PAD_ECSPI1_MOSI__UART3_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
 };
 
 static iomux_v3_cfg_t const wdog_pads[] = {
@@ -54,7 +54,7 @@ int board_early_init_f(void)
 
 	imx_iomux_v3_setup_multiple_pads(uart_pads, ARRAY_SIZE(uart_pads));
 
-	init_uart_clk(1);
+	init_uart_clk(2);
 
 	return 0;
 }
