31040ec2cae45c4973e8643c83269932b4e96b3c
Darren Huang <darren.huang@advantech.com.tw>
2022-03-31 16:57:12 +0800

    [ROM-7720] Enable the RTL8211F lan phy ethernet

--- include/configs/imx8qm_rom7720.h
+++ include/configs/imx8qm_rom7720.h
@@ -79,7 +79,7 @@
 
 #if (CONFIG_FEC_ENET_DEV == 0)
 #define IMX_FEC_BASE			0x5B040000
-#define CONFIG_FEC_MXC_PHYADDR          0x4
+#define CONFIG_FEC_MXC_PHYADDR          0x1
 #define CONFIG_ETHPRIME                 "eth0"
 #elif (CONFIG_FEC_ENET_DEV == 1)
 #define IMX_FEC_BASE			0x5B050000
