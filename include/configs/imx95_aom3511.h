59418d436ecba6bc91675625899f1033920f6cc0
SeanChuang2 <dinner0828@gmail.com>
2024-08-21 16:58:25 +0800

    [AOM-3511] Support aom-3511 8G DDR

--- include/configs/imx95_aom3511.h
+++ include/configs/imx95_aom3511.h
@@ -194,6 +194,12 @@
 #define PHYS_SDRAM_2_SIZE 		0x380000000 /* 14GB */
 #endif
 
+#if defined(CONFIG_TARGET_IMX95_AOM3511A1_8G)
+/* Totally 8GB */
+#define PHYS_SDRAM_SIZE			0x70000000 /* 2GB  - 256MB DDR */
+#define PHYS_SDRAM_2_SIZE		0x180000000 /* 6GB */
+#endif
+
 #define CFG_SYS_FSL_USDHC_NUM	2
 
 /* Using ULP WDOG for reset */
