81612df08cd89750f1ea96a441fb4c360176639e
kuihong.xin <kuihong.xin@advantech.com.cn>
2025-07-03 09:29:25 +0800

    [rs08] delete env (usblan_addr)

--- include/configs/imx8mm_ebcrs08.h
+++ include/configs/imx8mm_ebcrs08.h
@@ -113,7 +113,7 @@
 	"mmcpart=1\0" \
 	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
 	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot} usblan_addr=${eth1addr}\0 " \
+	"mmcargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot}\0 " \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bsp_script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
