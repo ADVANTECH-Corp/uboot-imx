a1ddd94fb6fe9f6f125d0a00a10fe363b3a1d274
Hank.Hsueh <Hank.Hsueh@advantech.com.tw>
2025-07-10 15:11:36 +0800

    [imx8mp][all][drivers] Update regulator enable logic in MMC
    Replaced regulator_set_enable_if_allowed with regulator_set_enable
    Adjusted error handling from ENOSYS to EACCES

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -2776,10 +2776,9 @@ static int mmc_power_on(struct mmc *mmc)
 {
 #if CONFIG_IS_ENABLED(DM_MMC) && CONFIG_IS_ENABLED(DM_REGULATOR)
 	if (mmc->vmmc_supply) {
-		int ret = regulator_set_enable_if_allowed(mmc->vmmc_supply,
-							  true);
+		int ret = regulator_set_enable(mmc->vmmc_supply, true);
 
-		if (ret && ret != -ENOSYS) {
+		if (ret && ret != -EACCES) {
 			printf("Error enabling VMMC supply : %d\n", ret);
 			return ret;
 		}
@@ -2793,10 +2792,9 @@ static int mmc_power_off(struct mmc *mmc)
 	mmc_set_clock(mmc, 0, MMC_CLK_DISABLE);
 #if CONFIG_IS_ENABLED(DM_MMC) && CONFIG_IS_ENABLED(DM_REGULATOR)
 	if (mmc->vmmc_supply) {
-		int ret = regulator_set_enable_if_allowed(mmc->vmmc_supply,
-							  false);
+		int ret = regulator_set_enable(mmc->vmmc_supply, false);
 
-		if (ret && ret != -ENOSYS) {
+		if (ret && ret != -EACCES) {
 			pr_debug("Error disabling VMMC supply : %d\n", ret);
 			return ret;
 		}
